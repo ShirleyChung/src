499fd1ccbd2a73d55eda4e2c2dc3bd47b7320a8a
Shirley Chung <jonesr_tw@yahoo.com.tw>
2017-04-02 09:53:47 +0800

    add ref cnt

--- classes/singleton.hpp
+++ classes/singleton.hpp
@@ -4,6 +4,7 @@
 template<class T>
 class Singleton
 {
+	static int _refCnt;
 	static T* _inst;
 	Singleton(Singleton const&);
 	Singleton& operator= (Singleton const&);
@@ -12,10 +13,22 @@ protected:
 	virtual ~Singleton();
 public:
 	static T* GetInstance(){
+		_refCnt++;
 		return _inst? _inst: (_inst = new T);
+	}
+
+	static int ReleaseInstance(){
+		_refCnt--;
+		if (_refCnt <0) _refCnt = 0;
+		if (!_refCnt){
+			delete _inst;
+			_inst = NULL;
+		}
+		return _refCnt;
 	}
 };
 
 template<class T> T* Singleton<T>::_inst = NULL;
+template<class T> int Singleton<T>::_refCnt = 0;
 
 #endif
