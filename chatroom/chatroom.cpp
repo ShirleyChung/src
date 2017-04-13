0cd800b5cb71ebae885feee2d9e07ab4ace5aaea
Shirley Chung <jonesr_tw@yahoo.com.tw>
2017-04-13 14:32:45 +0800

    modify chatroom

--- chatroom/chatroom.cpp
+++ chatroom/chatroom.cpp
@@ -1,1 +1,11 @@
 #include "chatroom.h"
+
+ChatRoom::ChatRoom()
+{
+	this_type::AddFunc("showmembers", &ChatRoom::ShowMembers);
+}
+
+void ChatRoom::ShowMembers(STRARR& cmd)
+{
+	cout <<" not impl. yet.\n";
+}
--- chatroom/chatroom.h
+++ chatroom/chatroom.h
@@ -1,12 +1,17 @@
 #ifndef _chat_room_socket_application_2017_04_sly_
 #define _chat_room_socket_application_2017_04_sly_
 
-#include "../classes/tcpserver.h"
+#include "../server/server.h"
 
-class ChatRoom: public TCPServer
+class ChatRoom: public Server, public FuncDisp<ChatRoom>
 {
+	typedef FuncDisp<ChatRoom> this_type;
+
+protected:
+	void ShowMembers(STRARR& cmd);
+
 public:
-	ChatRoom(){};
+	ChatRoom();
 	
 	virtual void OnConnect(string ip, int sck){};
 };
