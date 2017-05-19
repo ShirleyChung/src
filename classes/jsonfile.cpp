badddf62469d24ffc6a352af4df90d1ebc26909f
Shirley Chung <jonesr_tw@yahoo.com.tw>
2017-05-19 08:07:42 +0800

    add json

--- classes/jsonfile.cpp
+++ classes/jsonfile.cpp
@@ -1,1 +1,42 @@
 #include "jsonfile.h"
+#include <iostream>
+
+using namespace jsonfile;
+
+Object::Object(const string& content)
+{
+}
+
+Object::~Object()
+{
+}
+
+JSON::JSON(string& buf)
+{
+}
+
+JSON::JSON(const string& fn)
+{
+}
+	
+JSON::~JSON()
+{
+}
+
+/* 將檔案讀取到字串緩衝 */
+bool JSON::Read(const string& fn)
+{
+	ifstream ifs(fn.c_str());
+	if(!ifs){
+		cout<<"cannot read "<< fn <<NL;
+		return false;
+	}
+
+	string line;
+	getline(ifs, line); //xml info
+
+	while(getline(ifs, line))
+		_buf += line;
+
+	return true;
+}
--- classes/jsonfile.h
+++ classes/jsonfile.h
@@ -1,13 +1,48 @@
 #ifndef _json_parser_and_build_key_wvlue_truee_2017_05_
 #define _json_parser_and_build_key_wvlue_truee_2017_05_
 
-#include <string>
+#include <fstream>
 #include <map>
+#include <list>
+#include "stringparser.h"
 
 using namespace std;
 
-class JSON
+
+namespace jsonfile{
+const string LPP = "{";
+const string RPP = "}";
+const string LBP = "[";
+const string RBP = "]";
+const string NL  = "\n";
+
+
+class Object{
+protected:
+typedef map<string, Object> ITEM;
+
+	list<Object*> _items;
+public:
+	Object(const string& content);
+	virtual ~Object();
+};
+
+class JSON: private StringParser
 {
+protected:
+	void _DoParse(Object* parent);
+public:
+
+	JSON(string& buf);
+	JSON(const string& fn);
+	
+	virtual ~JSON();
+	
+	bool Read(const string& fn);
+	bool Save();
+	void ShowTree();
+};
+
 };
 
 #endif
